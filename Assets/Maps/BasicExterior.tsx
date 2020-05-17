0c4af8ba2089dbea93c744150e9fb3b32e578998
Abbi <>
2020-05-17 18:37:57 +0100

    remove old tiles, Tiled resources in Maps directory, rendered with help of SuperTiled2Unity

---
+++ Assets/Maps/BasicExterior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="BasicExterior" tilewidth="16" tileheight="16" tilecount="512" columns="8">
+ <image source="basic_exterior.png" width="128" height="1024"/>
+</tileset>
